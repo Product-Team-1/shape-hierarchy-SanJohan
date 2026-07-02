9d055f19415cea700dd14836a1aa3d20c1bb5f3d
SanJohan <johansantiago56@gmail.com>
2026-07-02 11:51:29 -0500

    implementando Shape interface y las clases con getArea

--- src/main/java/com/example/project/ShapeHierarchy.java
+++ src/main/java/com/example/project/ShapeHierarchy.java
@@ -5,5 +5,19 @@ import java.util.ArrayList;
 public class ShapeHierarchy {
     public static void main(String[] args) {
         // TODO: create a list/array of shapes and print each area (polymorphism)
+
+        ArrayList<Shape> listaObjetos = new ArrayList<>();
+
+        Circle circle = new Circle(1);
+        Rectangle rectangle = new Rectangle(2,3);
+        Triangle triangle = new Triangle(3,4);
+
+        listaObjetos.add(circle);
+        listaObjetos.add(rectangle);
+        listaObjetos.add(triangle);
+
+        for(Shape objeto : listaObjetos){
+            System.out.println(objeto.getArea());
+        }
     }
 }
